a4765b5ca7c3b3e8c1e1ce5327c8319e14c53072
MahmoudAladwy <mahmoudaladwy774@gmail.com>
2025-12-15 12:01:44 +0200

    - pull request test part 2

--- src/main/java/com/mycompany/clientxogame/App.java
+++ src/main/java/com/mycompany/clientxogame/App.java
@@ -32,7 +32,7 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-        System.out.println("test pull request");
+        System.out.println("test pull request part 2");
         launch();
     }
 
