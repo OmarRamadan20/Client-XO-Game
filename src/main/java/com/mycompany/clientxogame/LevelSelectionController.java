7d7b33013f2c762864d10568ba87416836155f96
amr abdelrahem <amr042277@gmail.com>
2025-12-20 01:16:23 +0200

    implement controller

---
+++ src/main/java/com/mycompany/clientxogame/LevelSelectionController.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.clientxogame;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+/**
+ * FXML Controller class
+ *
+ * @author amr04
+ */
+public class LevelSelectionController implements Initializable {
+
+
+    @FXML
+    private Button btmُُEasy;
+    @FXML
+    private Button btmMeduim;
+    @FXML
+    private Button btmHard;
+    @FXML
+    private Button btmBack;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void onActionBtmEasy(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtmMedium(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionHard(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBack(ActionEvent event) {
+    }
+
+}
