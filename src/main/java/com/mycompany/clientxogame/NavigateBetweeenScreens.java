1b4fed8f86dd1320356d0ad1a0a086ca37443d5c
Ahlam <129538648+Ahlamgomaa@users.noreply.github.com>
2025-12-20 01:17:29 +0200

    implemet methods navigation

--- src/main/java/com/mycompany/clientxogame/NavigateBetweeenScreens.java
+++ src/main/java/com/mycompany/clientxogame/NavigateBetweeenScreens.java
@@ -33,35 +33,98 @@ public class NavigateBetweeenScreens {
     public static void goToSingleMode(ActionEvent event) {
         changeScene(event, "/com/mycompany/clientxogame/LevelSelection.fxml", "Level Selection");
     }
-              public static void backToModeSelection(ActionEvent event)
-    {
-        changeScene(event, "/com/mycompany/clientxogame/    ModeSelection.fxm", "Mode Selection");
+
+    public static void backToModeSelection(ActionEvent event) {
+        changeScene(event, "/com/mycompany/clientxogame/ModeSelection.fxml", "Mode Selection");
     }
+
     public static void goToEasyLevel(ActionEvent event) {
-        changeScene(event, "/game/board.game", "XO Easy Level");
+        changeScene(event, "/game/board.fxml", "XO Easy Level");
     }
 
     public static void goToMeduimLevel(ActionEvent event) {
-        changeScene(event, "/game/board.game", "XO Meduim Level");
+        changeScene(event, "/game/board.fxml", "XO Meduim Level");
     }
 
     public static void goToHardLevel(ActionEvent event) {
-        changeScene(event, "/game/board.game", "XO Hard Level");
+        changeScene(event, "/game/board.fxml", "XO Hard Level");
     }
-            public static void backToLevelSelection(ActionEvent event)
-    {
-        changeScene(event, "/com/mycompany/clientxogame/LevelSelection.fxm", "Level Selection");
+
+    public static void backToLevelSelection(ActionEvent event) {
+        changeScene(event, "/com/mycompany/clientxogame/LevelSelection.fxml", "Level Selection");
     }
 
     public static void goToDoubleMode(ActionEvent event) {
-        changeScene(event, "/game/board.game", "XO");
+        changeScene(event, "/UI/players/offline_players.fxml", "Offline Players");
+    }
+
+    public static void goToPlay(ActionEvent event) {
+        changeScene(event, "/game/board.fxml", "Play Offline");
+    }
+
+    public static void backToOfflinePlayer(ActionEvent event) {
+        changeScene(event, "/UI/players/offline_players.fxml", "Offline Players");
     }
 
     public static void goToSignUp(ActionEvent event) {
-        changeScene(event, "/Register/signUp.fxml", "Sign Up");
+        changeScene(event, "/UI/Register/signUp.fxml", "Sign Up");
     }
 
     public static void goToLogIn(ActionEvent event) {
-        changeScene(event, "/Register/login.fxml", "Login");
+        changeScene(event, "/UI/Register/login.fxml", "Login");
     }
+
+    public static void goToAvailblePlayerAfterLogin(ActionEvent event) {
+        changeScene(event, "/UI/players/available_players.fxml", "Available Players");
+    }
+//ui not exist
+
+    public static void goToShowProfile(ActionEvent event) {
+        changeScene(event, "/UI/players/available_players.fxml", "Available Players");
+    }
+
+    public static void logOut(ActionEvent event) {
+        changeScene(event, "/UI/Register/signUp.fxml", "Sign Up");
+    }
+
+    public static void invite(ActionEvent event) {
+        changeScene(event, "/com/mycompany/clientxogame/accept.fxml", "Invite To Play");
+    }
+
+    public static void acceptToPlay(ActionEvent event) {
+        changeScene(event, "/game/board.fxml", "XO Game");
+    }
+
+    public static void rejectInvitation(ActionEvent event) {
+        changeScene(event, "/UI/players/available_players.fxml", "Available Players");
+    }
+
+    public static void cancelInvitation(ActionEvent event) {
+        changeScene(event, "/UI/players/available_players.fxml", "Available Players");
+    }
+
+    public static void backFromProfile(ActionEvent event) {
+        changeScene(event, "/UI/players/available_players.fxml", "Available Players");
+    }
+
+    public static void goToShowRecords(ActionEvent event) {
+        changeScene(event, "/com/mycompany/clientxogame/win.fxml", "Win");
+    }
+
+    public static void winGame(ActionEvent event) {
+        changeScene(event, "/com/mycompany/clientxogame/win.fxml", "Win");
+    }
+
+    public static void loseGame(ActionEvent event) {
+        changeScene(event, "/com/mycompany/clientxogame/lose.fxml", "Lose");
+    }
+
+    public static void goToPlayAgain(ActionEvent event) {
+        changeScene(event, "/game/board.fxml", "XO Game");
+    }
+
+    public static void goToNewGame(ActionEvent event) {
+        changeScene(event, "/game/board.fxml", "XO Game");
+    }
+
 }
