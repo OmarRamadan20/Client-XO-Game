37ae56edeb6426b9e7cfb363f181a7ea820e6df5
amr abdelrahem <amr042277@gmail.com>
2025-12-20 04:45:39 +0200

    handle controller

---
+++ src/main/java/com/mycompany/clientxogame/signUpController.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.clientxogame;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+/**
+ * FXML Controller class
+ *
+ * @author amr04
+ */
+public class signUpController implements Initializable {
+
+
+    @FXML
+    private TextField enterName;
+    @FXML
+    private TextField enterEmail;
+    @FXML
+    private TextField enterPassword;
+    @FXML
+    private TextField confirmPass;
+    @FXML
+    private Button btnBack;
+    @FXML
+    private Button btnSignUp;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void ActionBack(ActionEvent event) {
+    }
+
+    @FXML
+    private void signUp(ActionEvent event) {
+    }
+
+}
