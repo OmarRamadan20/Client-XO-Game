134d56d6984b4adf952571105ea85ec253a9dc41
Omar <omarramadan1222@gmail.com>
2025-12-20 18:35:44 +0200

    Save local changes before pull

--- src/main/java/com/mycompany/clientxogame/App.java
+++ src/main/java/com/mycompany/clientxogame/App.java
@@ -10,7 +10,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/game/board.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/com/mycompany/clientxogame/ModeSelection.fxml"));
         Scene scene = new Scene(root, 700,700);
         primaryStage.setTitle("XO Game");
         primaryStage.setScene(scene);
