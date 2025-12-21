984d03fe3d0f15218eb6a99afd186a2b68c88139
amr abdelrahem <amr042277@gmail.com>
2025-12-21 23:45:30 +0200

    implemnt hendel requset and response by thread

--- src/main/java/com/mycompany/clientxogame/ServerHandler.java
+++ src/main/java/com/mycompany/clientxogame/ServerHandler.java
@@ -4,39 +4,77 @@
  */
 package com.mycompany.clientxogame;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.Socket;
+import javafx.application.Platform;
 import org.json.JSONObject;
 
 /**
  *
  * @author amr04
  */
-class ServerHandler {
+ 
 
-    public ServerHandler() {
-        Socket mySocket;
-        try {
-            mySocket = new Socket("127.0.0.1", 5555);
-            PrintStream ps = new PrintStream(mySocket.getOutputStream());
-            DataInputStream dis;
-            dis = new DataInputStream(mySocket.getInputStream());
-            JSONObject request = new JSONObject();
-            request.put("type", "login");
-            request.put("gmail","amr042277@gmail.com");
-            request.put("password", "1234");
+public class ServerHandler extends Thread {
 
-            ps.println(request.toString());
-            String replyMsg = null;
-            replyMsg = dis.readLine();
+    private static ServerHandler instance;
 
-            System.out.println(replyMsg);
-        } catch (IOException ex) {
-            System.getLogger(ServerHandler.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+    private Socket socket;
+    private BufferedReader reader;
+    private PrintWriter writer;
+
+    private ServerListener listener;
+
+     private ServerHandler() {}
+
+     public static ServerHandler getInstance() {
+        if (instance == null) {
+            instance = new ServerHandler();
+            instance.start();
         }
-
+        return instance;
     }
 
+    @Override
+    public void run() {
+        try {
+            socket = new Socket("127.0.0.1", 5555);
+
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer = new PrintWriter(socket.getOutputStream(), true);
+
+             while (true) {
+                String msg = reader.readLine();
+                if (msg == null) break;
+                handleMessage(msg);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Disconnected from server");
+            e.printStackTrace();
+        }
+    }
+
+     public void send(JSONObject json) {
+        if (writer != null) {
+            writer.println(json.toString());
+        }
+    }
+
+     private void handleMessage(String msg) {
+        JSONObject json = new JSONObject(msg);
+        if (listener != null) {
+             Platform.runLater(() -> listener.onMessage(json));
+        }
+    }
+
+     public void setListener(ServerListener listener) {
+        this.listener = listener;
+    }
 }
+ 
