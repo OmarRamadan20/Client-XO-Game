3f7f0081fb9906a0c6f8fa0aaa86c0c8461c012b
Ahlam <129538648+Ahlamgomaa@users.noreply.github.com>
2025-12-22 00:28:52 +0200

    feat: enhance sign-up process with validation and server response handling

--- src/main/java/com/mycompany/clientxogame/signUpController.java
+++ src/main/java/com/mycompany/clientxogame/signUpController.java
@@ -12,13 +12,14 @@ import javafx.fxml.Initializable;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import org.json.JSONObject;
+
 /**
  * FXML Controller class
  *
  * @author amr04
  */
 public class signUpController implements Initializable {
-
 
     @FXML
     private TextField enterName;
@@ -32,22 +33,57 @@ public class signUpController implements Initializable {
     private Button btnBack;
     @FXML
     private Button btnSignUp;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
     @FXML
     private void ActionBack(ActionEvent event) {
-         NavigateBetweeenScreens.goToLogIn(event);
+        NavigateBetweeenScreens.goToLogIn(event);
     }
 
     @FXML
     private void signUp(ActionEvent event) {
-         NavigateBetweeenScreens.goToAvailablePlayer(event);
+        String name = enterName.getText();
+        String gmail = enterEmail.getText();
+        String password = enterPassword.getText();
+        String confirmPassword = confirmPass.getText();
+        if (name.isEmpty() || gmail.isEmpty() || password.isEmpty()) {
+            System.out.println("Please Fill All Fields");
+            return;
+        }
+        if (!password.equals(confirmPassword)) {
+            System.out.println("Passwords do not match");
+            return;
+        }
+        JSONObject request = new JSONObject();
+        request.put("type", "signup");
+        request.put("name", name);
+        request.put("gmail", gmail);
+        request.put("password", password);
+        ServerHandler.getInstance().setListener(new ServerListener() {
+            @Override
+            public void onMessage(JSONObject msg) {
+                String type = msg.getString("type");
+                if (type.equals("signup")) {
+                    String status = msg.getString("status");
+                    if (status.equals("success")) {
+                        System.out.println("Sign Up Is Sucessfull !");
+                        NavigateBetweeenScreens.goToAvailablePlayer(event);
+                    } else {
+                        System.out.println("Sign Up Is Failes !");
+                    }
+                }
+            }
+
+        });
+        ServerHandler.getInstance().send(request);
+
     }
 
 }
