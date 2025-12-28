729e9424c8c07ed98c9c4f21638973149bf086d8
Ahlam <129538648+Ahlamgomaa@users.noreply.github.com>
2025-12-28 15:38:39 +0200

    Refactor media handling in WinController and LoseController; update action formatting in nbactions.xml

--- src/main/java/com/mycompany/clientxogame/LoseController.java
+++ src/main/java/com/mycompany/clientxogame/LoseController.java
@@ -2,6 +2,7 @@ package com.mycompany.clientxogame;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -10,69 +11,80 @@ import javafx.scene.control.Label;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
-import javafx.util.Duration;
+import org.json.JSONObject;
 
 public class LoseController implements Initializable {
 
-    @FXML
-    private Label result;
-
-    @FXML
-    private Button newGameButton;
-
-    @FXML
-    private Button playAgainButton;
-
-    @FXML
-    private MediaView mediaView;
-
-    private MediaPlayer mediaPlayer;
+    @FXML private Label result;
+    @FXML private Button newGameButton;
+    @FXML private Button playAgainButton;
+    @FXML private MediaView mediaView;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         try {
-             URL videoUrl = getClass().getResource("/videos/lose.mp4");
+            URL videoUrl = getClass().getResource("/videos/lose.mp4");
+            if (videoUrl != null) {
+                Media media = new Media(videoUrl.toExternalForm());
+                MediaPlayer mediaPlayer = new MediaPlayer(media);
+                mediaView.setMediaPlayer(mediaPlayer);
 
-            if (videoUrl == null) {
-                 return;
+                mediaView.setFitWidth(400);
+                mediaView.setFitHeight(300);
+                mediaView.setPreserveRatio(true);
+
+                mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+                mediaPlayer.play();
             }
 
-            Media media = new Media(videoUrl.toExternalForm());
-            mediaPlayer = new MediaPlayer(media);
-            mediaView.setMediaPlayer(mediaPlayer);
-
-             mediaView.setFitWidth(400);
-            mediaView.setFitHeight(300);
-            mediaView.setPreserveRatio(true);
-
-             mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
-
-             mediaPlayer.setOnReady(() -> {
-                 mediaPlayer.play();
+          
+            ServerHandler.getInstance().setListener(json -> {
+                if (json.optString("type").equals("invite_recieved")) {
+                    Platform.runLater(() -> {
+              
+                        if (mediaView.getMediaPlayer() != null) {
+                            mediaView.getMediaPlayer().stop();
+                        }
+                        NavigateBetweeenScreens.invite(null, json.getString("from"));
+                    });
+                }
             });
 
-             mediaPlayer.setOnError(() -> {
-             });
-
         } catch (Exception e) {
-             e.printStackTrace();
+            e.printStackTrace();
         }
     }
 
     @FXML
     private void newGame(ActionEvent event) {
-        stopVideo();
-     }
+ 
+        if (mediaView.getMediaPlayer() != null) {
+            mediaView.getMediaPlayer().stop();
+        }
+        NavigateBetweeenScreens.goToAvailablePlayer(event);
+    }
 
     @FXML
     private void playAgain(ActionEvent event) {
-        stopVideo();
-     }
+ 
+        if (mediaView.getMediaPlayer() != null) {
+            mediaView.getMediaPlayer().stop();
+        }
 
-    private void stopVideo() {
-        if (mediaPlayer != null) {
-            mediaPlayer.stop();
-            mediaPlayer.dispose();  
+        String opponentName = NavigateBetweeenScreens.currentOpponent;
+        if (opponentName != null) {
+            JSONObject inviteReq = new JSONObject();
+            inviteReq.put("type", "invite");
+            inviteReq.put("from", LoggedUser.name);
+            inviteReq.put("to", opponentName);
+
+            ServerHandler.getInstance().send(inviteReq);
+
+        
+            NavigateBetweeenScreens.invitedFrom = opponentName; 
+            NavigateBetweeenScreens.goToWaitAccept(event);
+        } else {
+            NavigateBetweeenScreens.goToAvailablePlayer(event);
         }
     }
 }
--- src/main/java/com/mycompany/clientxogame/WinController.java
+++ src/main/java/com/mycompany/clientxogame/WinController.java
@@ -10,10 +10,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import org.json.JSONObject;
 
 /**
  * FXML Controller class
@@ -22,40 +22,62 @@ import javafx.scene.media.MediaView;
  */
 public class WinController implements Initializable {
 
-    @FXML
-    private Button newGameButton;
-    @FXML
-    private Button playAgainButton;
-    @FXML
-    private MediaView mediaView;
+    @FXML private Button newGameButton;
+    @FXML private Button playAgainButton;
+    @FXML private MediaView mediaView;
+    
+    private MediaPlayer mediaPlayer; 
 
-    /**
-     * Initializes the controller class.
-     */
-@Override
-public void initialize(URL url, ResourceBundle rb) {
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        try {
+            String path = getClass().getResource("/videos/win.mp4").toExternalForm();
+            Media media = new Media(path);
+            mediaPlayer = new MediaPlayer(media);
+            mediaView.setMediaPlayer(mediaPlayer);
+            mediaPlayer.play();
 
-    String path = getClass()
-            .getResource("/videos/win.mp4")
-            .toExternalForm();
-
-    Media media = new Media(path);
-    MediaPlayer mediaPlayer = new MediaPlayer(media);
-
-    mediaView.setMediaPlayer(mediaPlayer);
-
-    mediaPlayer.play();   
-}
+           
+            ServerHandler.getInstance().setListener(json -> {
+                if (json.optString("type").equals("invite_recieved")) {
+                    javafx.application.Platform.runLater(() -> {
+                        stopVideo();
+                        NavigateBetweeenScreens.invite(null, json.getString("from"));
+                    });
+                }
+            });
+        } catch (Exception e) { e.printStackTrace(); }
+    }
 
     @FXML
     private void newGame(ActionEvent event) {
-    
-        NavigateBetweeenScreens.goToNewGame(event);
-    
+        stopVideo();
+        NavigateBetweeenScreens.goToAvailablePlayer(event);
     }
 
     @FXML
     private void playAgain(ActionEvent event) {
+        stopVideo();
+        String opponentName = NavigateBetweeenScreens.currentOpponent;
+        if (opponentName != null) {
+            JSONObject inviteReq = new JSONObject();
+            inviteReq.put("type", "invite");
+            inviteReq.put("from", LoggedUser.name);
+            inviteReq.put("to", opponentName);
+            ServerHandler.getInstance().send(inviteReq);
+
+            NavigateBetweeenScreens.invitedFrom = opponentName; 
+            NavigateBetweeenScreens.goToWaitAccept(event);
+        } else {
+            NavigateBetweeenScreens.goToAvailablePlayer(event);
+        }
     }
-    
+
+    private void stopVideo() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.dispose();
+            mediaPlayer = null; 
+        }
+    }
 }
